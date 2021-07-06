21756a5aaeb60fefbf8e98707829e9af5a30aac8
tanb <bing.tan@topplusvision.com>
2021-07-06 15:37:56 +0800

    feat: update npm

--- docs/.vuepress/dist/service-worker.js
+++ docs/.vuepress/dist/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "1086d5b8a98d184be7c603eaacbe31c7"
+    "revision": "e31a6300f4f736b9d2f1cf97dca6f97a"
   },
   {
     "url": "assets/css/0.styles.09c98605.css",
@@ -38,60 +38,60 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.e34915f5.js",
-    "revision": "cc31e6a9c5d4f9d37b51ea8c5c294891"
+    "url": "assets/js/10.8372af8e.js",
+    "revision": "4d0026a356a754b7934976b6fc0a6da8"
   },
   {
-    "url": "assets/js/11.d349ba96.js",
-    "revision": "144ffc3cb40d42b10be9bcbe5580c89a"
+    "url": "assets/js/11.1287d40d.js",
+    "revision": "056cb718fd422a61889d0679af900ab4"
   },
   {
     "url": "assets/js/12.a6bcaef8.js",
     "revision": "c9398413b91c633b448f62d0e45d7cdb"
   },
   {
-    "url": "assets/js/13.9b2b12c6.js",
-    "revision": "640accefd8b294b6bb197cd6fbfac290"
+    "url": "assets/js/13.0b8da40c.js",
+    "revision": "8f9e7248a47fcac2eb2889fefae06c8c"
   },
   {
-    "url": "assets/js/14.b59af8bb.js",
-    "revision": "6a917b73228c3491b356d4296cb7ae07"
+    "url": "assets/js/14.d28cad14.js",
+    "revision": "b77bef1e40e80dfa7d8865e71f2cd276"
   },
   {
-    "url": "assets/js/15.a40e17e6.js",
-    "revision": "5567bee75eee6f27e473e9484279743e"
+    "url": "assets/js/15.ad281302.js",
+    "revision": "700b74c217956c8316eca31b1218e727"
   },
   {
     "url": "assets/js/16.ab4f9cac.js",
     "revision": "4d4efa1f98fad44e3e2584124dbd0edd"
   },
   {
-    "url": "assets/js/17.f8b56660.js",
-    "revision": "8d24a2b448a27156b5716085e6e0bcef"
+    "url": "assets/js/17.59a7c788.js",
+    "revision": "456b042560d896350123ddb28196db46"
   },
   {
-    "url": "assets/js/18.1413b7be.js",
-    "revision": "b6ca4c31d2ce343ea58a8bc338849ded"
+    "url": "assets/js/18.bdd4852b.js",
+    "revision": "8285c13de0d367c70a3fa646d6a0f55b"
   },
   {
-    "url": "assets/js/19.e166a5e6.js",
-    "revision": "2e328961351ddfeeac7e43d35e6621d1"
+    "url": "assets/js/19.cf42774b.js",
+    "revision": "8718bdcdc882aca876f4ffd93cc69ba9"
   },
   {
     "url": "assets/js/2.3e22a156.js",
     "revision": "22057eb18a2ed58f6b4b81efb825353b"
   },
   {
-    "url": "assets/js/20.ff8e2625.js",
-    "revision": "0fed20a2590e0e2e340b967b9e12ffd7"
+    "url": "assets/js/20.2a3e7b9a.js",
+    "revision": "a775347c0c364ad07143bba72873c569"
   },
   {
-    "url": "assets/js/21.0ae1e828.js",
-    "revision": "d03aba9db4da0cd3b7d0cdc1473eb1d3"
+    "url": "assets/js/21.eb6692a3.js",
+    "revision": "261ac415f91dab7ce0b9ee34d27917fd"
   },
   {
-    "url": "assets/js/22.c9981e67.js",
-    "revision": "7d404024cdd8095b87a77a56067e783e"
+    "url": "assets/js/22.22a118f1.js",
+    "revision": "4e4ed0f4ae44c81a8d3d4fda96eaa504"
   },
   {
     "url": "assets/js/23.7c9523a8.js",
@@ -126,60 +126,60 @@ self.__precacheManifest = [
     "revision": "3cdc17aef2b578729c98d40a5a6f8e77"
   },
   {
-    "url": "assets/js/9.de8c1b06.js",
-    "revision": "be1663943c6aefe6b34456f74a35b607"
+    "url": "assets/js/9.1ae2101f.js",
+    "revision": "21ae9231d5d1468d0a3498f4edd18811"
   },
   {
-    "url": "assets/js/app.e98e3124.js",
-    "revision": "8260d8e7e137136aca60b7a1804bbeff"
+    "url": "assets/js/app.ad225322.js",
+    "revision": "52f505de78161a43abac5498ded85c60"
   },
   {
     "url": "framework/cli/index.html",
-    "revision": "0da0387f3968c0d07425a5c845a27813"
+    "revision": "7879fdf6051517008e52a38e358d5372"
   },
   {
     "url": "framework/cli/vue.html",
-    "revision": "022ac9af22c5c11750aa3f25534ef0f5"
+    "revision": "3b5d4aeed177c3a9076919bdc5f3fbe9"
   },
   {
     "url": "framework/introduction/index.html",
-    "revision": "4005090b1fc81707699e5b1b736bf672"
+    "revision": "c987ddf71705e101fac918af728f75c5"
   },
   {
     "url": "git/index.html",
-    "revision": "8774cc607b368531037f171d648f192e"
+    "revision": "6a9657e015c3c32c40bb3151c40b473b"
   },
   {
     "url": "index.html",
-    "revision": "5b2ff5142aee7ecbb477fb5e36943848"
+    "revision": "df66e520ecbb582f56bd6648fdeb2ba9"
   },
   {
     "url": "interview/base/index.html",
-    "revision": "058a372d912923d65bfedb12a4f8825d"
+    "revision": "3fa0ba86014b5b90f838fafce5ce0a7d"
   },
   {
     "url": "interview/browser/http.html",
-    "revision": "c66b84378e312905387dfcf0597735e0"
+    "revision": "abc5e232605cef938302869494e32344"
   },
   {
     "url": "interview/browser/index.html",
-    "revision": "457b6645ad04fa116d8f99cf29e6d693"
+    "revision": "00262d01fe228e28d24b5a9fca75fae5"
   },
   {
     "url": "javascript/algorithm/index.html",
-    "revision": "09af262825339c0ed1937e7dab0209a6"
+    "revision": "4f272f48ad113b051597a826224d2879"
   },
   {
     "url": "javascript/es6/index.html",
-    "revision": "4aab6ad44d44d3a5a59486e0f8952d99"
+    "revision": "16c49bdc6c9d024698eead60d3a472e5"
   },
   {
     "url": "javascript/utils/index.html",
-    "revision": "034873946c6d9070bc142f1fad85f940"
+    "revision": "d4e1af55178415134e6e4d5084a74a98"
   },
   {
     "url": "javascript/utils/regexp.html",
-    "revision": "d07c8518a814ac7cf6f2d2d5b4ec9a8d"
+    "revision": "bd8826598d2257bc2951c9e5d6210aa1"
   },
   {
     "url": "logo.jpeg",
@@ -187,19 +187,19 @@ self.__precacheManifest = [
   },
   {
     "url": "nodejs/npm/index.html",
-    "revision": "f6ca4e2d2a7c7a277a11ee3675cb4e15"
+    "revision": "6b6ee3a3fd57dae439d53ebe1e329952"
   },
   {
     "url": "style/css/index.html",
-    "revision": "1b29c145db9892280493843981d20503"
+    "revision": "774aab1b693b0438c530997d9894ff75"
   },
   {
     "url": "visual/three/index.html",
-    "revision": "917de9406ace650af320d0a9f77253a1"
+    "revision": "2b3f0f5ee7b1c45f00ae2920a6251469"
   },
   {
     "url": "vue/index.html",
-    "revision": "1854cce1663738564930ccccfb4d9e4d"
+    "revision": "d10aea32ae769200d21ddceb319887cd"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- docs/nodejs/npm/README.md
+++ docs/nodejs/npm/README.md
@@ -2,20 +2,47 @@
 sidebar: auto
 ---
 
-# npm 
+## npm 
 
-查看当前 npm 源
 ```sh
+# 查看当前 npm 源
 npm config get registry
-# http://registry.npmjs.org/
-```
-
-切换源
-```sh
 # 淘宝源
 npm config set registry=https://registry.npm.taobao.org
+# yarn config set registry https://registry.npm.taobao.org
 # 官方源
 npm config set registry=http://registry.npmjs.org
+```
+
+## 查看已安装的依赖包
+```sh
+# 当前项目
+npm list --depth 0
+
+# 全局
+npm list -g --depth 0
+
+# yarn
+yarn global list --depth=0
+```
+
+## 查看依赖包的安装路径
+```sh
+# 当前项目
+npm root
+
+# 全局
+npm root -g
+
+# yarn
+yarn global dir
+```
+
+## 清除缓存
+```sh
+npm cache clean -f
+# OR
+yarn cache clean
 ```
 
 
@@ -44,38 +71,41 @@ npm login
 获取当前正在运行的脚本名称 `process.env.npm_lifecycle_event`
 
 
-# 常用的 npm 包
-
-> 只介绍安装方法和常用命令，具体可点击 github 查看详情
-
-## nrm
-
-npm registry 管理工具，能够查看和切换当前使用的 registry
-
+# nvm
 ```sh
-# 安装
-npm install -g nrm
+# 设置 node 镜像
+nvm node_mirror https://npm.taobao.org/mirrors/node/
 
-# 查看所有 registry
-nrm ls
-# 切换 registry
-nrm use cnp
+# 设置 npm 镜像
+nvm npm_mirror https://npm.taobao.org/mirrors/npm/
 ```
 
-[Gihub](https://github.com/Pana/nrm)
 
-## npm-check-updates
-
-更新 package.json 中的依赖包
-
+#### 安装和设置 - Mac
 ```sh
-# 安装
-npm install -g npm-check-updates
-
-# 检查依赖包版本
-ncu
-# 升级依赖包版本
-ncu -u
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+# OR
+curl -o- https://gitee.com/mirrors/nvm/raw/v0.37.2/install.sh | bash
 ```
 
-[Gihub](https://github.com/tjunnone/npm-check-updates)
+#### 常用命令
+
+```sh
+# 查看 nvm 版本
+nvm version
+
+# 安装最新版的 node
+nvm install latest
+
+# 安装指定版本的 node
+nvm install 版本号
+
+# 卸载指定版本的 node
+nvm uninstall 版本号
+
+# 查看已安装的 node 版本
+nvm list
+
+# 切换到指定的 node 版本
+nvm use 版本号
+```
